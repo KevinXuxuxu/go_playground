031d181539a44fd15884b4c88632af04e6e22624
KevinXuxuxu <kevin.xu.fangzhou@gmail.com>
2020-02-01 23:37:58 -0800

    finished bubble sort

--- bubble_sort.go
+++ bubble_sort.go
@@ -1,8 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	a := []int{1, 2, 3, 4, 5, 6, 7}
+	a := []int{6, 3, 4, 2, 5, 7, 10000, 2, 3422, 123, 12341234, 234, 22, 341234}
+	for i := len(a) - 1; i > 0; i-- {
+		for j := 0; j < i; j++ {
+			if a[j] > a[j+1] {
+				a[j], a[j+1] = a[j+1], a[j]
+			}
+		}
+	}
 	fmt.Println(a)
 }
